dd668a99e83899d3f11e35ae5ddb918051de9229
Andrew Patterson <ampatte2@asu.edu>
2024-08-29 13:41:02 -0700

    Contribution Data added

--- app/tldr/page.tsx
+++ app/tldr/page.tsx
@@ -1,50 +1,89 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { parse, differenceInCalendarYears, differenceInCalendarMonths } from 'date-fns';
+import { fr } from 'date-fns/locale';
+
 import { Button } from "@/components/ui/button";
 import { Column } from "@/components/ui/flex";
+import React from 'react';
+import { ApiResponse, retrieveContributionData } from "../../util/api/github-contributions";
 
-export default function TLDR() {
+const formatDate = Intl.DateTimeFormat([], { dateStyle: "medium" });
+interface TimelineData {
+  startDate: Date;
+  endDate: Date;
+  content: string;
+  title: string;
+}
+
+interface TimelineProps {
+  data: TimelineData[];
+}
+
+const formatEmployedLength = (start: Date, end: Date) => {
+  const monthsDifference = differenceInCalendarMonths(end, start);
+  const months = monthsDifference % 12;
+  const years = Math.floor(monthsDifference / 12);
+  const monthCopy = months > 0 ? `${months} months` : "";
+  const yearCopy = years > 0 ? `${years} years` : "";
+  return `${yearCopy} ${monthCopy}`
+}
+
+const Timeline = (props: TimelineProps) => {
+  return (
+    <div className="flex flex-col">
+      {props.data.map((item, index) => (
+        <div key={index} className="timeline-item">
+          <div className="timeline-circle"></div>
+          <div className="timeline-content">
+            <h2>{item.title}</h2>
+            <h4 className="mt-2">{formatDate.format(item.startDate)} to {formatDate.format(item.endDate)}</h4>
+            <h4>{formatEmployedLength(item.startDate, item.endDate)}</h4>
+            <p className="mt-2">{item.content}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const data = [
+  { startDate: new Date('2018 01 01'), endDate: new Date('2019 06 01'), title: 'Procleos Labs', content: 'Trading application that was focused on individual investors' },
+  { startDate: new Date('2019 09 01'), endDate: new Date('2021 05 01'), title: 'Cheaper Eats', content: 'Cross platform application to service restaurants ordering and back office software' },
+  { startDate: new Date('2021 05 '), endDate: new Date(), title: 'Bondlink', content: 'Front end and back end for municipalities to manage and disseminate information about Bond Sales and their communities' },
+];
+
+export default async function TLDR(props: { res: ApiResponse }) {
+  const res = await retrieveContributionData("Ampatte2");
+
   return <Column classes="gap-1">
     <h1>Andrew M. Patterson, Solutions Specialist</h1>
     <h2>Software Engineer, UAS Pilot, Inventor</h2>
-    <p>I don't believe in problems, only challenges that haven't been solved yet.</p>
+    <p>I don't believe in problems, only challenges that haven't been solved.</p>
     <h3>Skills</h3>
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Functional Programming</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>User Interfaces and User Experience</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Object Oriented Programming</AccordionTrigger>
-        <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-4">
-        <AccordionTrigger>Hacking</AccordionTrigger>
-        <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-5">
-        <AccordionTrigger>Component Library Maintenance and Extension</AccordionTrigger>
-        <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-    <Button>Here</Button>
+    <ol>
+      <li>Eagle Scout</li>
+      <li>Team Oriented</li>
+      <li>Consistent and dependable team member</li>
+      <li>Team work makes the dream work</li>
+      <li>Willing to take responsibility, set an example, and lead</li>
+      <li>Revels in a good challenge</li>
+      <li>Thinks in terms of solutions rather than problems</li>
+    </ol>
+    <h3>Professional History</h3>
+    <>
+      After getting straight A's and pre-med requirements to become a Psychiatrist I realized that wasn't for me.
+      I took a few years and taught myself how to program, and fell in love with the challenges.
+      In order to get my foot in the door I took a junior position at a trading company startup.
+      Quickly I rose to be one of the best performing juniors, but the company did not secure additional funding.
+      My next opportunity arose at a Food Service startup within a year I was offered a promotion and a raise, and when the company began to expand I was promoted to Team Lead.
+      I was in charge of managing the "Core" Epic, which encompased a variety of projects and routine maintenance on the site.
+      Before long I was spear-heading month long features managing a team of five.
+    </>
+    <div className="flex flex-row">
+      <Timeline data={data} />
+    </div>
+    <Button>Download Resume</Button>
+    <Button>Full Site</Button>
     <p>
       With over eight years of experience in the software engineering landscape, I bring a deep-seated expertise in crafting sophisticated solutions that drive innovation and efficiency. My journey through the world of web technologies and computer science has equipped me with a broad and versatile skill set, allowing me to tackle a diverse range of challenges and deliver impactful results.
     </p>
---
+++ util/api/github-contributions.ts
@@ -0,0 +1,57 @@
+export type ContributionDay = {
+  contributionCount: number
+  date: string
+}
+
+export type ApiResponse = {
+  data: {
+    user: {
+      contributionsCollection: {
+        contributionCalendar: {
+          totalContributions: number
+          weeks: {
+            contributionDays: ContributionDay[]
+          }[]
+        }
+      }
+    }
+  }
+}
+
+
+const query = `
+query($userName:String!) {
+  user(login: $userName){
+    contributionsCollection {
+      contributionCalendar {
+        totalContributions
+        weeks {
+          contributionDays {
+            contributionCount
+            date
+          }
+        }
+      }
+    }
+  }
+}
+`
+export async function retrieveContributionData(userName: string): Promise<ApiResponse> {
+  const variables = `
+  {
+    "userName": "${userName}"
+  }
+`
+  const body = {
+    query,
+    variables
+  }
+  const res = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    },
+    body: JSON.stringify(body)
+  })
+  return res.json()
+}
